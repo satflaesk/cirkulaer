3cf4c1e9eabfc2cbe852c78f13ee2ad2a12c9374
satflaesk <32981187+satflaesk@users.noreply.github.com>
2025-12-05 14:18:24 +0800

    Make scripts run from anywhere inside work tree

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
+# To automatically run this script before pushing, navigate to the root directory of the repository
+# and issue the following sequence of commands:
+#
+# $ mkdir --parents .git/hooks
+# $ cd .git/hooks
+# $ ln --symbolic ../../scripts/pre-push.sh pre-push
+
 set -eoux pipefail;
 
-./scripts/build-all-targets.sh;
-./scripts/check-coverage.sh;
-./scripts/check-example-in-readme-file.sh;
-./scripts/check-formatting.sh;
-./scripts/generate-documentation.sh;
-./scripts/run-all-examples.sh;
-./scripts/run-all-tests.sh;
-./scripts/run-clippy.sh;
+repo_root=$(git rev-parse --show-toplevel);
+pushd "${repo_root}/scripts";
+
+./build-all-targets.sh;
+./check-coverage.sh;
+./check-example-in-readme-file.sh;
+./check-formatting.sh;
+./generate-documentation.sh;
+./run-all-examples.sh;
+./run-all-tests.sh;
+./run-clippy.sh;
+
+popd;
