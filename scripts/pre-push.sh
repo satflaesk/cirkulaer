938cf4203213127a4dadf8e5a1802cae83a0f00b
satflaesk <32981187+satflaesk@users.noreply.github.com>
2026-02-02 20:53:47 +0800

    Start using Miri
    To prevent UB from being introduced.

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -16,7 +16,9 @@ pushd "${repo_root}/scripts";
 ./check-coverage.sh;
 ./check-formatting.sh;
 ./generate-documentation.sh;
+./run-all-examples-under-miri.sh;
 ./run-all-examples.sh;
+./run-all-tests-under-miri.sh;
 ./run-all-tests.sh;
 ./run-clippy.sh;
 
