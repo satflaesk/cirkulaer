110a3120c2392c883995f0115884b7524fd9ffc7
satflaesk <32981187+satflaesk@users.noreply.github.com>
2026-02-15 08:21:21 +0800

    Do unchecked arithmetic where possible
    Some addition and subtraction operations are guaranteed not to overflow
    or underflow, hence we need not pay for such checks.

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@
 #![doc = include_str!("../README.md")]
 #![feature(generic_const_exprs)]
 
+mod circular_index;
+pub use crate::circular_index::CircularIndex;
+
 mod error;
 pub use crate::error::ValueError;
 
 mod trait_bound;
 pub use crate::trait_bound::{Bool, True, is_strictly_positive};
-
-mod circular_index;
-pub use crate::circular_index::CircularIndex;
