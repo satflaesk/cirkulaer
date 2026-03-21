8996f5e0bed8c5fff08cb11448f6f5a8fce1fb96
satflaesk <32981187+satflaesk@users.noreply.github.com>
2026-03-21 17:39:03 +0800

    Set `RUSTFLAGS` and `RUSTDOCFLAGS` in the scripts
    Since the scripts are used both by the pre-commit hook and in CI, it
    makes sense to set the `RUSTFLAGS` and `RUSTDOCFLAGS` environment
    variables in these.
    
    Also, one of the documentation scripts is removed, because the
    documentation string for the only private item being documented is
    hidden, so the script here removed brings no value.

--- .githooks/pre-commit.sh
+++ .githooks/pre-commit.sh
@@ -11,8 +11,7 @@ pushd "${repo_root}/scripts";
 
 ./check-coverage.sh;
 ./check-formatting.sh;
-./generate-all-documentation.sh;
-./generate-public-documentation.sh;
+./generate-documentation.sh;
 ./run-all-examples-under-miri.sh;
 ./run-all-examples.sh;
 ./run-all-tests-under-miri.sh;
